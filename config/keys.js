f63388c58589f6f5a20611348d6bc4f67bd3357f
hananwanii <104060914+hananwanii@users.noreply.github.com>
2023-10-13 04:36:45 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MongoURI: 'mongodb://localhost/habit_tracker'    
+    MongoURI: 'mongodb+srv://hananwanii:l1ZKGvUJaajqI2ln@cluster0.hgbajox.mongodb.net/?retryWrites=true&w=majority'    
 }
